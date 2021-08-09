a0c445ed23ec8870c715d1c1776e9a887ca972c9
Javier G. Leal <jgleal@gmail.com>
2021-08-09 13:22:50 +0200

    feat: add queries

--- queries.js
+++ queries.js
@@ -6,4 +6,18 @@ const translationsOfPackaesInPeriod = (from, to, ids)=> ({
 
 const wellFormedEdifyId = { edifyId: { $regex: /[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/ } }
 
-module.exports = { translationsOfPackaesInPeriod, wellFormedEdifyId }
+const recordsWithoutMapping = from => ({ 
+    "errors.message": "Package that requires new Ids with no mapping file found. Original msg: Unexpected status code: 404",
+    timestamp: { $gte: ISODate(from) }
+})
+
+const serachByEdifyPackagePath = packageId => ({
+     edifyPackagePath: `/${packageId}/`
+})
+
+module.exports = {
+    translationsOfPackaesInPeriod,
+    recordsWithoutMapping,
+    serachByEdifyPackagePath,
+    wellFormedEdifyId
+}
